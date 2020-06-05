55e56bcb5a6af0f2f9d23bb9add585df4ff5d577
Lucas Paulo <lucaspaulodev@gmail.com>
2020-06-05 15:34:45 -0300

    Sending the latest version of the server
    Sending the latest version of the ecoleta application server with authentication and images upload

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -18,8 +18,15 @@ class PointsController {
             .distinct()
             .select('points.*');
 
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,
+                image_url: `http://192.168.0.11:3333/uploads/${point.image}`,
+            };
+        });
 
-        return res.json(points);
+
+        return res.json(serializedPoints);
     }
 
     async show (req: Request,res: Response){
@@ -31,12 +38,19 @@ class PointsController {
             return res.status(400).json({message: 'Point not found'});
         }
 
+        const serializedPoint = {
+                ...point,
+                image_url: `http://192.168.0.11:3333/uploads/${point.image}`,
+            };
+
+
+
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
             .where('point_items.point_id', id)
             .select('items.title')
 
-        return res.json({point, items});
+        return res.json({point: serializedPoint, items});
     }
 
     async create(req: Request,res: Response){
@@ -55,7 +69,7 @@ class PointsController {
         const trx = await knex.transaction();
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: req.file.filename,
             name,
             email,
             whatsapp,
@@ -69,7 +83,10 @@ class PointsController {
     
         const point_id = insertedIds[0]
     
-        const pointItems = items.map((item_id: number) => {
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
             return{
                 item_id,
                 point_id
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import routes from './routes';
 import path from 'path';
 import cors from 'cors'
+import {errors} from 'celebrate'
 
 const app = express();
 
 app.use(cors()) //controle de quais urls porderão utilizar a API da aplicação
 app.use(express.json());
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors());
 
 app.listen(3333);
